9b397d28fd9a357789d8d89c42ec261131244bd7
PURU Puru <puru.puru@soprasteria.com>
2026-06-10 23:59:10 +0530

    ProductBugFix

--- Product.java
+++ Product.java
@@ -1,5 +1,18 @@
 package p1.entity;
 
 public class Product {
-
+ private String ProductId;
+ private String ProductName;
+ public String getProductId() {
+	return ProductId;
+ }
+ public void setProductId(String productId) {
+	ProductId = productId;
+ }
+ public String getProductName() {
+	return ProductName;
+ }
+ public void setProductName(String productName) {
+	ProductName = productName;
+ }
 }
