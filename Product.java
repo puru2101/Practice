301a526abdb489da2ff4e32fe2e1c699f1b967e4
PURU Puru <puru.puru@soprasteria.com>
2026-06-11 00:03:01 +0530

    Added quantity in Product

--- Product.java
+++ Product.java
@@ -3,6 +3,13 @@ package p1.entity;
 public class Product {
  private String ProductId;
  private String ProductName;
+ private long quantity;
+ public long getQuantity() {
+	return quantity;
+}
+ public void setQuantity(long quantity) {
+	this.quantity = quantity;
+ }
  public String getProductId() {
 	return ProductId;
  }
